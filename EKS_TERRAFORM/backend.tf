81dc4b0f7a70510bd252c4d880b9e2b3e7d704f6
Sai <Sai.com>
2025-12-18 10:50:44 +0530

    Revert "Update AZ"
    This reverts commit 799a7057cd541ddcfbd61285cc00f60cace19124.

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-sai-738" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-reyaz-111" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
